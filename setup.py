ada21199aa9ab15b218a9442f289824d54f70103
Vincent Crevot <vincent+git@retroject.net>
2014-07-09 05:27:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+scripts = []
+scripts.append('find_images=outscale_factory_buildbot.tools.find_images:main')
+scripts.append('get_image=outscale_factory_buildbot.tools.get_image:main')
+scripts.append('delete_images=outscale_factory_buildbot.tools.delete_images:main')
+scripts.append('gen_password=outscale_factory_buildbot.tools.gen_password:main')
+
+setup(
+    name='outscale_factory_buildbot',
+    version='0.1',
+    description='Buildmaster support package',
+    url='http://github.com/nodalink/outscale-factory-buildbot',
+    author='Vincent Crevot',
+    author_email=None,
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points=dict(console_scripts=scripts),
+    install_requires=['boto'],
+)

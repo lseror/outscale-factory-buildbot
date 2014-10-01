907c9dafd9c943ae301e826335c94721959d69ca
Aurélien Gâteau <mail@agateau.com>
2014-10-01 12:31:38 +0000

    Adjust logging setup so that it can be called earlier
    This is necessary to get logging configured by the time we fetch repos from the
    marketplace.

--- outscale_factory_buildbot/buildbot/log.py
+++ outscale_factory_buildbot/buildbot/log.py
@@ -11,7 +11,7 @@ def _loglevel_string_to_object(loglevel_string):
     return getattr(logging, loglevel_string)
 
 
-def configure_logging(c, fc, repos, meta):
+def configure_logging(c, fc):
     """
     Configure logging.
     """
@@ -19,6 +19,6 @@ def configure_logging(c, fc, repos, meta):
     string_boto = fc.get('loglevel_boto', 'INFO')
     level_default = _loglevel_string_to_object(string_boto)
     level_boto = _loglevel_string_to_object(string_default)
-    logging.basicConfig(format='%(levelname)s:%(message)s',
+    logging.basicConfig(format='%(levelname)s:%(name)s: %(message)s',
                         level=level_default)
     logging.getLogger('boto').setLevel(level_boto)
